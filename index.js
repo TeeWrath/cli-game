c195672f10aa273d0c9f0ad95ec1f06a246559bb
TeeWrath <subroto.2003@gmail.com>
2024-02-26 12:06:15 +0530

    feat: askname, game rules, welcome message

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 #!/usr/bin/env node
 // The above code is a shabang which determines that this is a cli tool
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+let player;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome(){
+    const rainbowTitle = chalkAnimation.rainbow(`Who wants to be a millionare? \n`);
+
+    await sleep();
+    rainbowTitle.stop();
+
+    console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')}
+    I am a process on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+    `);
+}
+
+async function askName(){
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'What is your name?',
+        default() {
+            return 'Player';
+        },
+    });
+
+    player = answers.player_name;
+}
+
+await welcome();
+await askName();
